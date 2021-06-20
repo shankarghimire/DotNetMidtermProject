548bcbba01bd40b88555ceb93f1c0d03397e9899
shankar Ghimire <ssghimire@gmail.com>
2021-06-20 05:45:40 -0400

    Completes Update and Delete operation on Airline

--- Airline.cs
+++ Airline.cs
@@ -59,6 +59,7 @@ namespace TakeHomeMidterm
     {
         Chicken,
         Sushi,
-        Salad
+        Salad,
+        None
     }
 }
--- AirlineWindow.xaml.cs
+++ AirlineWindow.xaml.cs
@@ -66,17 +66,95 @@ namespace TakeHomeMidterm
 
         private void btnUpdate_Click(object sender, RoutedEventArgs e)
         {
+            //bool result = false;
+            try
+            {
+                int aId = int.Parse(tbAirlineId.Text);
+                var airlineToUpdate = de.airlineList.Single(air => air.Id == aId);
 
+                string airlineName = tbAirlineName.Text;
+                string airPlane = tbAirplane.Text;
+                int aSeatsAvailable = int.Parse( tbAvailableSeats.Text);
+                string aMeal = tbMealAvailable.Text;
+                MealAvailable aMealAvailable = MealAvailable.None;
+                switch (aMeal)
+                {
+                    case "Chicken":
+                        aMealAvailable = MealAvailable.Chicken;
+                        break;
+                    case "Sushi":
+                        aMealAvailable = MealAvailable.Sushi;
+                        break;
+                    case "Salad":
+                        aMealAvailable = MealAvailable.Salad;
+                        break;
+                    default:
+                        MessageBox.Show("Invalid value for the Meal Available :", "Data Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                        break;
+                }
+                
+                airlineToUpdate.Name = airlineName;
+                airlineToUpdate.Airplane = airPlane;
+                airlineToUpdate.SeatAvailable = aSeatsAvailable;
+                airlineToUpdate.MealAvailable = aMealAvailable;
+
+                //de.customerList.Remove(customerToRemove);
+                var airlineData = from airline in de.airlineList
+                               select airline.Id + "\t" + airline.Name + "\t" + airline.Airplane + "\t" + airline.SeatAvailable + "\t" + airline.MealAvailable;
+
+                lstAirlines.DataContext = airlineData;
+                //result = true;
+            }
+            catch (Exception ex)
+            {
+                //result = false;
+            }
         }
 
         private void btnAddNew_Click(object sender, RoutedEventArgs e)
         {
 
+            if (tbAirlineId.Text == "" || tbAirlineName.Text == "" || tbAirplane.Text == "" || tbAvailableSeats.Text == "" || tbMealAvailable.Text == "")
+            {
+                MessageBox.Show("All the fields are mandatory!", "Data Validation Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                int aId = int.Parse(tbAirlineId.Text);
+                string aName = tbAirlineName.Text;
+                string  aAirplane= tbAirplane.Text;
+                string aSeatsAvailable = tbAvailableSeats.Text;
+                string aMealAvailable = tbMealAvailable.Text;
+                de.customerList.Add(new Customer(aId, aName, aAirplane, aSeatsAvailable, aMealAvailable));
+                var airlineData = from airline in de.airlineList
+                               select airline.Id + "\t" + airline.Name + "\t" + airline.Airplane + "\t" + airline.SeatAvailable + "\t" + airline.MealAvailable;
+
+                lstAirlines.DataContext = airlineData;
+            }
+
+
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                int aId = int.Parse(tbAirlineId.Text);
+                var airlineToDelete = de.airlineList.Single(air => air.Id == aId);
 
+                de.airlineList.Remove(airlineToDelete);
+
+                //de.customerList.Remove(customerToRemove);
+                var airlineData = from airline in de.airlineList
+                                  select airline.Id + "\t" + airline.Name + "\t" + airline.Airplane + "\t" + airline.SeatAvailable + "\t" + airline.MealAvailable;
+
+                lstAirlines.DataContext = airlineData;
+                //result = true;
+            }
+            catch (Exception ex)
+            {
+                //result = false;
+            }
         }
     }
 }
